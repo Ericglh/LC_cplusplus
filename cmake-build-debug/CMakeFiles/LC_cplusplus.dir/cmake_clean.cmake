174f82171ddfb8c52299fa831c87597265e3c040
Lianghao Gao <676835848@qq.com>
2018-04-03 10:17:59 -0700

    set up Clion

---
+++ cmake-build-debug/CMakeFiles/LC_cplusplus.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LC_cplusplus.dir/main.cpp.o"
+  "LC_cplusplus.pdb"
+  "LC_cplusplus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LC_cplusplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
